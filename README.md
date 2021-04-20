7d72367ef760a0087eefca91d4d4ef617654ef43
scutxd <scutxd@gmail.com>
2021-04-20 17:36:16 +0800

    add ignore files and build batch file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # q-dir_ahk
+build.bat build .exe file which does not need autohotkey installed
---
+++ build.bat
@@ -0,0 +1,21 @@
+@echo off
+SET AHKPath=D:\Program Files\AutoHotkey
+REM Takes a simple optional command parameter, /t, which starts testing before build.
+SET Param1=%1
+
+if "%Param1%"=="/t" (
+    start /w "%AHKPath%\Autohotkey.exe" q-dir_ahk.ahk -quiet
+)
+REM Return code from above (0 if tests all pass) is stored in %errorlevel%
+if errorlevel 1 (
+    echo   .
+    echo   .
+    echo Tests failed. Exes not built. Log contents:
+    echo   .
+    echo   .
+    type testLogs\*
+    echo   .
+    echo   .
+    exit /b %errorlevel%
+)
+"%AHKPath%\compiler\ahk2exe.exe" /in q-dir_ahk.ahk /out q-dir_ahk.exe /compress 0
